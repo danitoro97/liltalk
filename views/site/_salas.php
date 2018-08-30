0f142f6afe9dc009480811a1574dca10e46c2f86
Daniel Toro Nieto <danitn1997@gmail.com>
2018-08-30 16:11:57 +0200

    algunos cambios

--- views/site/_salas.php
+++ views/site/_salas.php
@@ -4,6 +4,6 @@ use yii\helpers\Html;
 
  ?>
 
- <div class="col-md-3 index-salas">
+ <div class="col-md-3 index-salas text-truncate">
      <?=Html::a(Html::encode($model->sala->nombre), ['salas/view', 'id' => $model->sala_id]) ?>
  </div>
