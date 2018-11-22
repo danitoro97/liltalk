7b710e7c26d8a263c8feea945ebd710f1c6c09ba
Daniel Toro Nieto <danitn1997@gmail.com>
2018-11-22 16:47:51 +0100

    actionlogin

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -2,10 +2,30 @@
 
 namespace app\controllers;
 
+use Yii;
+
+use app\models\Usuarios;
+
 use yii\rest\ActiveController;
 
 class UsersController extends ActiveController
 {
     public $modelClass = 'app\models\Usuarios';
-    
+
+    /**
+     * Comprueba credenciales del usuario y devuelve sus datos si existe o falso si no existe
+     * @return [type] [description]
+     */
+    public function actionLogin()
+    {
+        if (!empty($_POST)) {
+            $nombre = $_POST['usuario'];
+            $password = $_POST['password'];
+
+            $usuario = Usuarios::findOne(['nombre' => $nombre]);
+            if ($usuario != null) {
+                return (Yii::$app->getSecurity()->validatePassword($password, $usuario->password)) ? $usuario: false;
+            }
+        }
+    }
 }
